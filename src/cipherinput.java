234d061965c0051a6edb5a1dad6f28472000f7db
Ryan Darst <ryandarst@gmail.com>
2015-11-04 14:38:46 -0800

    Initial Commit
    Initial commit to github

---
+++ src/cipherinput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class cipherinput {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input text to encrypt");
+		String input = sc.nextLine().toUpperCase();
+		System.out.println(input);
+		String code = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int length = input.length();
+		String output = "";
+		int location = 0;
+		System.out.println("Enter movement amount");
+		int adjust = sc.nextInt();
+		for(int i=1; i<=length; i++){
+			char temp = input.charAt(i - 1);
+			location = code.indexOf(temp) + 2;
+			output = code.substring(location + adjust - 1, location + adjust);
+			System.out.print(output);
+			
+			
+			
+			
+		}
+	}
+
+}
